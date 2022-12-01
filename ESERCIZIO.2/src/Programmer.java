14c40b473f6e371ad428fa77f964ce55bd4d140a
GiovannaFi <119609101+GiovannaFi@users.noreply.github.com>
2022-12-01 19:02:33 +0100

    Esercizio 2 completo

---
+++ ESERCIZIO.2/src/Programmer.java
@@ -0,0 +1,16 @@
+public class Programmer {
+    public String name;
+    public String programmingLanguage;
+    public int yearsOfExperience;
+
+    Programmer(){
+        this.name = name;
+        this.programmingLanguage = programmingLanguage;
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+
+    public void printProgrammerDetails(){
+        System.out.println(name + " " + programmingLanguage + " " + yearsOfExperience);
+    }
+}
